2ab944a5e14c28a267f1cfb67d7b87832652cf27
Nanda Ikhwanul <nanda.pertama465@gmail.com>
2024-10-29 17:13:04 +0700

    mengubah route dan dashboard di survey mahasiswa

--- resources/views/mahasiswa/survey.blade.php
+++ resources/views/mahasiswa/survey.blade.php
@@ -7,19 +7,16 @@
     <div class="flex-1 p-6 mx-auto max-w-6xl h-screen">
         <div>
             <h1 class="text-2xl font-bold text-gray-800 text-center font-poppins" data-aos="fade-up">SURVEY TERBARU</h1>
-
+            <p class="text-center col-span-3">Tidak ada survei aktif saat ini.</p>
             <!-- Dynamic Slate background box with 3 grid columns -->
             <div class="mt-6 p-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4" data-aos="fade-up">
-                @forelse($surveys as $survey)
                     <!-- Grid item for each survey -->
                     <div class="bg-white p-4 shadow-md rounded-lg h-44 text-center grid items-end" data-aos="zoom-in">
-                        <p class="text-gray-700 font-semibold">{{ $survey->nama }}</p>
-                        <p class="text-gray-500">{{ $survey->jenis }} - Semester {{ $survey->semester }} ({{ $survey->tahun }})</p>
-                        <a href="{{ route('mahasiswa.survey_detail', $survey->id) }}" class="text-slate-800 font-bold font-poppins bg-yellow-400 rounded-md py-1 tracking-wide hover:bg-yellow-500 hover:text-white">View</a>
+                        <p class="text-gray-700 font-semibold"></p>
+                        <p class="text-gray-500"></p>
+                        <a href="" class="text-slate-800 font-bold font-poppins bg-yellow-400 rounded-md py-1 tracking-wide hover:bg-yellow-500 hover:text-white">View</a>
                     </div>
-                @empty
-                    <p class="text-center col-span-3">Tidak ada survei aktif saat ini.</p>
-                @endforelse
+                    
             </div>
         </div>
     </div>
